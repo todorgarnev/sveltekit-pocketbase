5a9f1e07c835374cefd21c0b05e041c70d8839f7
Todor Garnev <tgarnev@abv.bg>
2023-02-12 21:21:11 +0200

    Added register server logic

--- web/src/lib/utils/utils.ts
+++ web/src/lib/utils/utils.ts
@@ -1,5 +1,12 @@
 import type { User } from "../types/types";
 
-export const serializeNonPOJOs = (obj: User) => {
+const { randomBytes } = await import("node:crypto");
+
+export const serializeNonPOJOs = (obj: User): User => {
 	return structuredClone(obj);
+};
+
+export const generateUsername = (name: string): string => {
+	const id: string = randomBytes(2).toString("hex");
+	return `${name.slice(0, 5)}${id}`;
 };
---
+++ web/src/routes/register/+page.server.ts
@@ -0,0 +1,21 @@
+import { generateUsername } from "$lib/utils/utils";
+import { error, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+	register: async ({ locals, request }) => {
+		const body = Object.fromEntries(await request.formData());
+
+		const username: string = generateUsername((body.name as string).split(" ").join("")).toLowerCase();
+
+		try {
+			await locals.pb.collection("users").create({ username, ...body });
+			await locals.pb.collection("users").requestVerification(body.email as string);
+		} catch (err) {
+			console.log(err);
+			throw error(500, "Something went wrong");
+		}
+
+		throw redirect(303, "/login");
+	}
+};
