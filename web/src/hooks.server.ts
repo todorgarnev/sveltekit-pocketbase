8d24c606e5af6d782d130ed804565b44b815ff0d
Todor Garnev <tgarnev@abv.bg>
2023-02-17 12:46:39 +0200

    Updated hook file logic

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -6,9 +6,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase("http://127.0.0.1:8090");
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
 
-	if (event.locals.pb.authStore.isValid) {
-		event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
-	} else {
+	try {
+		if (event.locals.pb.authStore.isValid) {
+			await event.locals.pb.collection("users").authRefresh();
+			event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
+		}
+	} catch (_) {
+		event.locals.pb.authStore.clear();
 		event.locals.user = undefined;
 	}
 
